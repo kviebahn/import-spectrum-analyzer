ea3dd09b2cf74318298c7ca77327782b11f8afa2
kviebahn <kv291@cam.ac.uk>
2016-02-04 15:27:56 +0000

    cleared up header treatment

--- ImportSpectrumAnalyzer.py
+++ ImportSpectrumAnalyzer.py
@@ -9,13 +9,13 @@ import matplotlib.pyplot as plt
 import csv
 import time
 
-my_title = 'AOM driver #1 signal'
+my_title = 'AOM driver SerNo #2 signal'
 
 my_date = time.strftime("%Y/%m/%d/")
 
 my_folder = 'AOM drivers/'
 
-fname = 'AOM_driver_1.csv'
+fname = 'AOM2.csv'
 
 datadir = '//Volumes/data/Shared/'
 
@@ -31,28 +31,38 @@ if importbool:
     my_file = open(datapath)
     reader = csv.reader(my_file, dialect='excel', delimiter=',')
     count = 0
-    my_len = 0
+    my_len = 2
     #skip first two rows
-
+    for i0 in xrange(58):
+        next(reader) 
     #import csv as numpy array, leaving out blank rows
     for row in reader:
         print row
-        print len(row)
-        if not str.isdigit(row[0][1]):
-            my_len = len(row)
+        if (count == 0):
+            a = np.append(a,np.array(row[:my_len]).astype(np.float))
+            count += 1
         else:
-            if (count == 0)*(str.isdigit(row[0][1]))*('' not in row[:my_len]):
-                a = np.append(a,np.array(row).astype(np.float))
-                count += 1
-            else:
-                if (len(row) == my_len)*('' not in row[:my_len]):
-                    a = np.vstack((a,np.array(row[:my_len]).astype(np.float)))
+            a = np.vstack((a,np.array(row[:my_len]).astype(np.float)))
     my_file.close()
     np.save(datapath[:-4] + '.npy',a)
 
 #once you imported the file to npy it is much quicker to just load the npy rather than reading the whole csv again:
 if loadnpy:
     a = np.load(datapath[:-4] + '.npy')
+
+# Make nice freq labels
+
+unitHzNo = np.int(np.floor(np.log10(a[-1,0])/3.))
+
+unitHzdict = {
+    0:'Hz',
+    1:'kHz',
+    2:'MHz',
+    3:'GHz'
+}
+
+freqlabel = 'Frequency/%s' % unitHzdict[unitHzNo]
+a[:,0]/=(10**(unitHzNo*3))
 
 # Make plot
 
@@ -61,26 +71,13 @@ fig = plt.figure(figsize = (16,9))
 ax = fig.add_subplot(111)
 
 plt.title(my_title)
-ax.set_xlabel('Scan/s')
-ax.set_ylabel('Oscilloscope trace/V')
+ax.set_xlabel(freqlabel)
+ax.set_ylabel('Power/dBm')
 
-#ax.plot(a[:,0], a[:,3], 'b-', label = 'Doppler-broadened absorption')
-#ax.plot(a[:,0], a[:,4], 'r-', label = 'Doppler-free absorption')
-#ax.plot(a[:,0], a[:,1], 'y-', label = 'Intensity signal dl pro/V')
-ax.plot(a[:,0], a[:,1], 'y-', label = 'AOM driver output')
+ax.plot(a[:,0], a[:,1], 'g-')#, label = 'AOM driver output')
 
-ax.legend(loc = 6)
+#ax.legend(loc = 0)
 
-
-ax_twin = ax.twinx()
-
-#ax_twin.plot(a[:,0], a[:,2], 'g-', label = 'Intensity signal CoSy/V')
-ax_twin.plot(a[:,0], a[:,2], 'g-', label = 'AOM driver monitor')
-ax_twin.set_ylabel('Oscilloscope trace/V')
-
-ax_twin.legend(loc = 7)
-
-#ax_twin.set_ylim(ymin = 2.4, ymax = 2.5)
 
 #plt.ylim(ymin = 40, ymax = 50)
 plt.xlim(xmin = a[0,0], xmax = a[-1,0])
